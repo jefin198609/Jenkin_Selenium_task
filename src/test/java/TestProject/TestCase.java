fa9ff3e52bee9b3d6f4c055ae8825c8952b0d455
jefin198609 <93194525+jefin198609@users.noreply.github.com>
2022-04-08 11:29:00 +0530

    Update TestCase.java

--- src/test/java/TestProject/TestCase.java
+++ src/test/java/TestProject/TestCase.java
@@ -1,16 +1,20 @@
 package TestProject;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		WebDriver driver = new ChromeDriver();
+		/*WebDriver driver = new ChromeDriver();
 		driver.get("http://google.com");
 		driver.get("https://jefin-dotnetapp.azurewebsites.net"); 
-		//driver.quit();
+		driver.quit();*/
+		System.setProperty("webdriver.chrome.driver","D:/UST-SELF_TRAINING/jenkin-selenium/chrome driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "https://jefin-dotnetapp.azurewebsites.net";
+		driver.get(baseUrl);
 
 	}
 
